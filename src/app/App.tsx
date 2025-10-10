5b7d1e33d6e1182e0e0872451befb87252864e2e
Muvahhid <Muvahhid@MacBook-Pro-1.local>
2025-10-10 21:36:46 +0300

    done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,13 @@ import "./App.scss";
 function App() {
   return (
     <>
+      <p>
+        Данные для входа: <br />
+        <br />
+        Email: admin@mail.ru
+        <br />
+        Password: admin
+      </p>
       <Authorization />
     </>
   );
